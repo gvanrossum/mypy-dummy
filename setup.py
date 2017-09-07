792780be3f97dab2ab9a2821308e359a5bb1f99e
Jukka Lehtosalo <jukka@dropbox.com>
2017-09-07 15:10:32 +0100

    Fix typo

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 setup(name='mypy-lang',
       version='0.5.0',
       description="Dummy to remind people to switch to 'pip install mypy'",
-      author="Gudo van Rossum",
+      author="Guido van Rossum",
       author_email='guido@python.org',
       license="MIT License",
       packages=['mypy'],
